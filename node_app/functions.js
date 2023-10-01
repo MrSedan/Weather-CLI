68358bf8272e1779952a586cc5411ded61cf1304
Sergey Elpashev <elpashevs@bk.ru>
2023-10-01 15:48:25 +0300

    JS Comments

--- node_app/functions.js
+++ node_app/functions.js
@@ -1,16 +1,20 @@
-import fetch from 'node-fetch';
+import fetch from 'node-fetch'; // Импорт функции веб-запроса
 
-
+// Функция получения погоды
 const currentWeather = async (city, units="m") => {
-
+    // Проверка ввода
     if (city.length > 0 || (units !== "F" && units !== "C"))  {
         let link = '';
+        
+        // Выбор необходимой ссылки в зависимости от выбранной единицы
         if (units !=="f" && units !=="F") {
             link = `http://api.weatherstack.com/current?access_key=${process.env.API}&query=${city}`
         } else {
             link = `http://api.weatherstack.com/current?access_key=${process.env.API}&units=f&query=${city}`
         } 
-        const response = await fetch(link)
+        const response = await fetch(link); // Запрос на API
+
+        // Парсинг ответа от сервера
         let data = await response.json();
         const weatherInfo = {
             name: data.location.name,
@@ -21,6 +25,7 @@ const currentWeather = async (city, units="m") => {
             condition: data.current.weather_descriptions[0]
         }
 
+        // Формирование текста вывода из полученных данных в удобный формат
         let weatherText=`
         ${weatherInfo.name} - ${weatherInfo.region} - ${weatherInfo.country}\n
         ###################Info:###################
@@ -32,11 +37,12 @@ const currentWeather = async (city, units="m") => {
 
         console.log(weatherText);
     } else {
-        console.log("Error: Write a name of a city or the right value of Units")
+        console.log("Error: Write a name of a city or the right value of Units"); // Вывод ошибки в случае неправильного ввода
     }
    
 }
 
+// Разрешение на использование функции в другом файле
 export  {
     currentWeather
 }
--- node_app/index.js
+++ node_app/index.js
@@ -1,19 +1,23 @@
+// Импортирование необходимых библиотек
 import program from 'commander';
 import dotenv from 'dotenv';
 
-
+// Импортирование функции из другого файла
 import {currentWeather} from './functions.js'
-dotenv.config({path: '../.env'})
+
+dotenv.config({path: '../.env'}) // Получение API ключа из файла .env
 
 
 
 
-program.version("1.0.0").description("Console Application for find the Weather of a city");
+program.version("1.0.0").description("Console Application for find the Weather of a city"); // Информация о программе
+
+// Информация и запуске программы с флагами
 program
     .command('current <city> <units>')
     .alias('c')
     .description('See the weather of the city, write F to get temperature in Fahrenheit, or C to get in Celcius')
     .action((city, units) => currentWeather(city, units))
 
-
+// Получение аргументов запуска
 program.parse(process.argv);
