4372afd29a100cdf4ff7da1f129a21660e21e80e
Sergey Elpashev <elpashevs@bk.ru>
2023-10-25 21:14:46 +0300

    Code refactor

--- node_app/functions.js
+++ node_app/functions.js
@@ -1,40 +1,45 @@
-import fetch from 'node-fetch'; // Импорт функции веб-запроса
+import fetch from "node-fetch"; // Импорт функции веб-запроса
 
 // Функция получения погоды
-export const currentWeather = async (city, units="m") => {
-    // Проверка ввода
-    if (city.length > 0 || (units !== "F" && units !== "C" && units !== 'K'))  {
-        // Выбор необходимой ссылки в зависимости от выбранной единицы
-        const link = `http://api.weatherstack.com/current?access_key=${process.env.API}&${units.toLowerCase()==='f'?'units=f&':''}query=${city}` 
-        const response = await fetch(link); // Запрос на API
-        let weatherInfo = {};
-        // Парсинг ответа от сервера
-        try {
-            let data = await response.json();
-            weatherInfo = {
-                name: data.location.name,
-                country: data.location.country,
-                region: data.location.region,
-                time: data.current.observation_time,
-                temperature: data.current.temperature + (units.toUpperCase()==='K'?273.15:0),
-                condition: data.current.weather_descriptions[0]
-            }
-        } catch {
-            throw new Error('You entered wrong city!')
-        }
+export const currentWeather = async (city, units = "m") => {
+  // Проверка ввода
+  if (city.length > 0 || (units !== "F" && units !== "C" && units !== "K")) {
+    // Выбор необходимой ссылки в зависимости от выбранной единицы
+    const link = `http://api.weatherstack.com/current?access_key=${
+      process.env.API
+    }&${units.toLowerCase() === "f" ? "units=f&" : ""}query=${city}`;
+    const response = await fetch(link); // Запрос на API
+    let weatherInfo = {};
+    // Парсинг ответа от сервера
+    try {
+      let data = await response.json();
+      weatherInfo = {
+        name: data.location.name,
+        country: data.location.country,
+        region: data.location.region,
+        time: data.current.observation_time,
+        temperature:
+          data.current.temperature + (units.toUpperCase() === "K" ? 273.15 : 0),
+        condition: data.current.weather_descriptions[0],
+      };
+    } catch {
+      throw new Error("You entered wrong city!");
+    }
 
-        // Формирование текста вывода из полученных данных в удобный формат
-        let weatherText=`
+    // Формирование текста вывода из полученных данных в удобный формат
+    let weatherText = `
         ${weatherInfo.name} - ${weatherInfo.region} - ${weatherInfo.country}\n
         ###################Info:###################
                   Time: ${weatherInfo.time}                       
                   Condition: ${weatherInfo.condition}             
-                  Temperature: ${weatherInfo.temperature} ${units.toUpperCase()}°              
+                  Temperature: ${
+                    weatherInfo.temperature
+                  } ${units.toUpperCase()}°              
         ###########################################
-        `
+        `;
 
-        console.log(weatherText);
-    } else {
-        console.log("Error: Write a name of a city or the right value of Units"); // Вывод ошибки в случае неправильного ввода
-    }  
-}
+    console.log(weatherText);
+  } else {
+    console.log("Error: Write a name of a city or the right value of Units"); // Вывод ошибки в случае неправильного ввода
+  }
+};
--- python_app/functions.py
+++ python_app/functions.py
@@ -1,42 +1,54 @@
-import os, sys
+import os
+import sys
+
 import requests
+
 
 # Очистка экрана
 def clear():
     command = "clear"
-    
+
     # Смена команды очистки, если запущено на Windows
     if os.name in ("nt", "dow"):
         command = "cls"
     os.system(command)
 
 # Приветственное сообщение и начало работы
+
+
 def menu():
     print("Welcome to the Weather Application!")
     print("press s to start the application")
     print("press q to quit!")
     answer = input("")
-    while(answer.lower() != "s" and answer.lower() != "q"):
+    while (answer.lower() != "s" and answer.lower() != "q"):
         print("Please write s or q")
     return answer.lower()
 
 # Получение необходимого города и единиц измерения
+
+
 def weatherMenu():
     city = input("Write the name of the City you looking for: ")
-    while len(city) <= 0:
+    while len(city) == 0:
         city = input("Write the name of the City you looking for: ")
-    units = input("Write F to choose Fahrenheit or C to choose Celsius or K to choose Kelvin: ")
-    while(len(units) <= 0 or (units.lower() != "f" and units.lower() != "c" and units.lower() != "k")):
-        units = input("You entered wrong letter. Write F to choose Fahrenheit or C to choose Celsius or K to choose Kelvin: ")
+    units = input(
+        "Write F to choose Fahrenheit or C to choose Celsius or K to choose Kelvin: ")
+    while (len(units) == 0 or (units.lower() != "f" and units.lower() != "c" and units.lower() != "k")):
+        units = input(
+            "You entered wrong letter. Write F to choose Fahrenheit or C to choose Celsius or K to choose Kelvin: ")
     return {"city": city.capitalize(), "units": units.upper()}
 
 # Получение погоды от API
+
+
 def getWeather(city, units, API):
     link = "http://api.weatherstack.com/current"
     params = {'access_key': API, 'query': city}
-    if units=='F': params['units'] = 'f'
+    if units == 'F':
+        params['units'] = 'f'
     r = requests.get(link, params=params)
-    
+
     # Парсинг ответа от API
     try:
         data = r.json()
@@ -44,7 +56,7 @@ def getWeather(city, units, API):
             "name": data["location"]["name"],
             "country": data["location"]["country"],
             "region": data["location"]["region"],
-            "temperature": data["current"]["temperature"]+ 273.15 if units=='K' else 0,
+            "temperature": data["current"]["temperature"] + 273.15 if units == 'K' else 0,
             "condition": data["current"]["weather_descriptions"][0]
         }
         printInfo(weatherInfo, units.upper())
@@ -53,6 +65,8 @@ def getWeather(city, units, API):
         sys.exit(0)
 
 # Вывод погоды в удобном для понимании виде
+
+
 def printInfo(info, units):
     text = f"""
         {info["name"]} - {info["region"]} - {info["country"]}
